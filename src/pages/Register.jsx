350f2a9f6d8633c534fb2e7efd18b4fce65437e1
Isaias Godoy <isaiasgodoy98@gmail.com>
2026-07-24 23:18:45 -0300

    Add Register page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,5 +1,55 @@
-function Register() {
-    return <h1>Register</h1>
+import { useState } from 'react'
+import { useNavigate, Link } from 'react-router-dom'
+
+function Register(){
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const [error, setError] = useState('')
+    const navigate = useNavigate()
+
+    const handleSubmit = async(e) => {
+        e.preventDefault()
+        setError('')
+
+        const response = await fetch('http://localhost:5196/api/auth/register', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ username, password })
+        })
+
+        if(response.ok) {
+            navigate('/login')
+        } else {
+            setError('There was an error. Please try again')
+        }
+    }
+
+    return (
+        <div>
+            <h1>Register</h1>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label>Username</label>
+                    <input
+                        type="text"
+                        value= {username}
+                        onChange={(e) => setUsername(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <label>Password</label>
+                    <input
+                        type="password"
+                        value={password}
+                        onChange={(e)=>setPassword(e.target.value)}
+                    />
+                </div>
+                {error && <p>{error}</p>}
+                <button type="submit">Register</button>
+            </form>
+            <p>Already have an account? <Link to="/login">Login</Link> </p>
+        </div>
+    )
 }
 
 export default Register
